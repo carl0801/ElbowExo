14167c17a4e97fa84fbeaa8ab799dd86692dfc2a
Dha2511 <50593834+Dha2511@users.noreply.github.com>
2024-09-30 14:11:23 +0200

    Updated exg to work with emg instead of imu

--- Shimmer3/connectShimmer.py
+++ Shimmer3/connectShimmer.py
@@ -2,23 +2,24 @@ import shimmer
 import util
 import numpy as np
 
-TYPE = util.SHIMMER_GSRplus
-PORT = "COM5"
+TYPE = util.SHIMMER_ExG_0
+PORT = "COM6"
 
 # Initialize Shimmer
 shimmer = shimmer.Shimmer3(TYPE, debug=True)
 shimmer.connect(com_port=PORT, write_rtc=True, update_all_properties=True, reset_sensors=True)
-shimmer.set_sampling_rate(512.0)
-shimmer.set_enabled_sensors(util.SENSOR_LOW_NOISE_ACCELEROMETER)
+shimmer.set_sampling_rate(512)
+shimmer.set_enabled_sensors(util.SENSOR_ExG1_16BIT, util.SENSOR_ExG2_16BIT)
 shimmer.print_object_properties()
+print(shimmer.get_available_sensors())
 
 shimmer.start_bt_streaming()
 
 # Initial buffer size (can be adjusted)
-INITIAL_CAPACITY = 1000
+INITIAL_CAPACITY = 10000
 
 # Pre-allocate numpy array to store the data
-data = np.empty((INITIAL_CAPACITY, 6))  # Assuming 6 columns (timestamp, RTCstart, RTCcurrent, sensor1, sensor2, time_diff)
+data = np.empty((INITIAL_CAPACITY, 3))  # Assuming 6 columns (timestamp, RTCstart, RTCcurrent, sensor1, sensor2, time_diff)
 current_index = 0
 
 # Dynamically resize function for the numpy array
@@ -27,21 +28,28 @@ def resize_array(arr, new_size):
 
 try:
     while True:
-        n_of_packets, packets = shimmer.read_data_packet_extended(calibrated=False)
+        n_of_packets, packets = shimmer.read_data_packet_extended() #calibrated=False)
         if n_of_packets > 0:
             for packet in packets:
-                # Calculate the time difference between RTCcurrent and RTCstart
-                time_diff = packet[2] - packet[1]
-
                 # Ensure the numpy array has enough space, dynamically resize if necessary
                 if current_index >= data.shape[0]:
-                    data = resize_array(data, data.shape[0] * 2)  # Double the size when more space is needed
+                    data = resize_array(data, data.shape[0] + 10000)
+
+                # Calculate the time stamp between RTCcurrent and RTCstart
+                timestamp = packet[2] - packet[1]
+                sensor1 = packet[3]
+                sensor2 = packet[4]
 
                 # Store the data (timestamp, RTCstart, RTCcurrent, sensor1, sensor2, time_diff)
-                data[current_index, :] = [packet[0], packet[1], packet[2], packet[3], packet[4], time_diff]
+                data[current_index, :] = [timestamp, sensor1, sensor2]
                 current_index += 1
 
+                if current_index % 1000 == 0:
+                    print(packet)
+                    print(f"Timestamp: {timestamp}, Sensor1: {sensor1}, Sensor2: {sensor2}")
+
 except KeyboardInterrupt:
+    print("KeyboardInterrupt")
     shimmer.stop_bt_streaming()
     shimmer.disconnect(reset_obj_to_init=True)
 
--- Shimmer3/displayShimmerData.py
+++ Shimmer3/displayShimmerData.py
@@ -7,19 +7,59 @@ data = np.load('shimmer_data.npz')['data']
 
 # Extract the timestamps and the GSR data
 timestamps = data[:, 0]
-timeRTCstart = data[:, 1]
-timeRTCcurrent = data[:, 2]
-sensor1_data = data[:, 3]
-sensor2_data = data[:, 4]
+sensor1_data = data[:, 1]
+sensor2_data = data[:, 2]
+
+# Perform high pass filtering
+from scipy.signal import butter, lfilter
+
+def butter_highpass(cutoff, fs, order=5):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
+
+def highpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_highpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+# Define the high pass filter parameters
+fs = 512
+cutoff = 5
+
+# Apply the high pass filter to the GSR data
+sensor1_data = highpass_filter(sensor1_data, cutoff, fs)
+sensor2_data = highpass_filter(sensor2_data, cutoff, fs)
+
+# Remove the first 2000 entries to remove the transient response
+timestamps = timestamps[1024:]
+sensor1_data = sensor1_data[1024:]
+sensor2_data = sensor2_data[1024:]
+
+# Absolute value of the data
+sensor1_data = np.abs(sensor1_data)
+sensor2_data = np.abs(sensor2_data)
+
+# Moving average filter
+def moving_average(data, window_size):
+    return np.convolve(data, np.ones(window_size) / window_size, mode='same')
+
+# Apply the moving average filter to the EMG data
+window_size = 100
+sensor1_data = moving_average(sensor1_data, window_size)
+sensor2_data = moving_average(sensor2_data, window_size)
 
 
-# Plot the GSR data, GSR stands for Galvanic Skin Response
 plt.figure()
 plt.plot(timestamps, sensor1_data, label='Sensor 1')
 plt.plot(timestamps, sensor2_data, label='Sensor 2')
 
+# Set y-axis range
+plt.ylim(0, 100)
+
 plt.xlabel('Timestamp')
-plt.ylabel('GSR Data')
+plt.ylabel('EMG Data')
 plt.legend()
 plt.show()
 
